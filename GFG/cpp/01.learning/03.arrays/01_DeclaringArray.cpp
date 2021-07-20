9141a6179fb5654228462c6c8a4971c0818b0190
krishnamurali1999 <82135780+krishnamurali1999@users.noreply.github.com>
2021-07-20 16:30:21 +0530

    uploaded

---
+++ GFG/cpp/01.learning/03.arrays/01_DeclaringArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[5];
+
+    int n=5;
+    int arr[n];
+
+    int arr[5] = {10, 20};
+    int arr[10] = {0};
+
+    int arr1[] {10,20,30};
+    int arr2[5] {10,20,30,40,50};
+    int arr3[5] {10, 20};
+
+    return 0;
+}
